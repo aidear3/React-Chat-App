8685c288810f50a59faba0f09ddcb61672f2d6fa
Senior Frontend Developer <javsfdev323@gmail.com>
2023-04-17 20:23:02 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginForm from './components/LoginForm';
 import './App.css';
 
 const projectID = '2c43c9d8-2a07-414e-b7b1-7b37cc4616c7';
+
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
 
